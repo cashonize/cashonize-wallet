42a7ebb331a363af099598e152a53b7588b9ca6d
Mathieu Geukens <mr-zwets@protonmail.com>
2024-02-01 20:26:30 +0100

    change default height window

--- src-electron/electron-main.ts
+++ src-electron/electron-main.ts
@@ -14,7 +14,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     icon: path.resolve(__dirname, 'icons/icon.png'), // tray icon
     width: 1000,
-    height: 600,
+    height: 750,
     useContentSize: true,
     autoHideMenuBar: true,
     webPreferences: {
