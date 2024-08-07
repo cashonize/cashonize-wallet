52890b3a87673902f2b04342768c197eaeaca34b
mainnet-pat <mainnet_pat@protonmail.com>
2024-08-07 11:50:00 +0000

    Better refresh behaviour of tokens page

--- src/components/myTokens.vue
+++ src/components/myTokens.vue
@@ -3,26 +3,17 @@
   import tokenItemNFT from './tokenItems/tokenItemNFT.vue'
   import tokenItemFT from './tokenItems/tokenItemFT.vue'
   import { useStore } from 'src/stores/store'
-  import { ref, watch } from 'vue';
 
   const store = useStore()
-
-  const bcmrRefreshKey = ref(0);
-  watch(store, () => {
-    if (store.bcmrRegistries === undefined) {
-      return;
-    }
-    bcmrRefreshKey.value++;
-  });
 </script>
 
 <template>
   <div v-if="store.nrBcmrRegistries == undefined" style="text-align: center;">Loading tokendata ...</div>
   <div v-if="store.tokenList?.length == 0" style="text-align: center;"> No tokens in this wallet </div>
-  <div v-if="store.nrBcmrRegistries != undefined" :key="bcmrRefreshKey">
+  <div v-if="store.nrBcmrRegistries != undefined" :key="(store.tokenList?.[0]?.tokenId ?? '') + (store.tokenList?.length ?? 0)">
     <div v-for="tokenData in store.tokenList" :key="tokenData.tokenId.slice(0,6)">
-      <tokenItemFT v-if="'amount' in tokenData" :tokenData="tokenData"/>
-      <tokenItemNFT v-else :tokenData="tokenData"/>
+      <tokenItemFT v-if="'amount' in tokenData" :tokenData="tokenData" :key="store.bcmrRegistries?.[tokenData.tokenId]?.name"/>
+      <tokenItemNFT v-else :tokenData="tokenData" :key="Object.values(store.bcmrRegistries?.[tokenData.tokenId]?.nfts ?? {}).length"/>
     </div>
   </div>
 </template>
