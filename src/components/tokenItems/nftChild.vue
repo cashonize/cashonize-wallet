c5ba3b93de76de52634062dfa27679a72da804ce
Mathieu Geukens <mr-zwets@protonmail.com>
2024-09-20 07:55:15 +0200

    delete commented out code from nftChild component

--- src/components/tokenItems/nftChild.vue
+++ src/components/tokenItems/nftChild.vue
@@ -243,12 +243,6 @@
       <legend>
         <div id="tokenType"></div>
       </legend>
-      <!--<div v-if="tokenData?.verified" id="verified" class="verified">
-        <div class="tooltip">
-          <img class="verifiedIcon" src="images/check-circle.svg">
-          <span class="tooltiptext">Verified</span>
-        </div>
-      </div>-->
       <div class="tokenInfo">
         <video v-if="httpsUrlTokenIcon?.endsWith('.mp4')" class="tokenIcon" style="width: 48px; height: 48px; border-radius: 50%;cursor: pointer;" @click="() => showNftImage = true">
           <source :src="httpsUrlTokenIcon" type="video/mp4" />
@@ -278,11 +272,6 @@
             <img id="burnIcon" class="icon" :src="settingsStore.darkMode? 'images/fireLightGrey.svg' : 'images/fire.svg'">
             <span class="hidemobile">burn NFT</span>
           </span>
-          <!--<span v-if="tokenData?.auth" style="white-space: nowrap;" id="authButton">
-            <img id="authIcon" class="icon" src="images/shield.svg">
-            <span class="hidemobile">auth transfer</span>
-            <span class="showmobile">auth</span>
-          </span>-->
         </div>
         <div v-if="displayNftInfo" id="tokenInfoDisplay" style="margin-top: 10px;">
           <div v-if="tokenMetaData?.description" id="tokenDescription"> NFT description: {{ nftDescription }} </div>
@@ -323,16 +312,6 @@
           <br>
           <input @click="burnNft()" type="button" id="burnNFT" value="burn NFT" class="button error">
         </div>
-        <!---<div id="authTransfer" class="hide" style="margin-top: 10px;">
-          Transfer the authority to change the token's metadata to another wallet <br>
-          This should be to a wallet with coin-control, where you can label the Auth UTXO<br>
-          It is recommended to use the Electron Cash pc wallet<br>
-          <br>
-          <span class="grouped">
-            <input id="destinationAddr" placeholder="destinationAddress"> 
-            <input type="button" id="transferAuth" value="Transfer Auth">
-          </span>
-        </div>-->
       </div>
     </fieldset>
     <div v-if="showNftImage && (nftMetadata?.uris?.image || nftMetadata?.uris?.icon)">
