0aade00becd8a0a523349dc430988ff35489d423
Mathieu Geukens <mr-zwets@protonmail.com>
2025-04-07 13:00:09 +0200

    modernize eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,9 +2,9 @@ import js from '@eslint/js'
 import globals from 'globals'
 import pluginVue from 'eslint-plugin-vue'
 import pluginQuasar from '@quasar/app-vite/eslint'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 
-export default [
+export default defineConfigWithVueTs(
   {
     /**
      * Ignore the following files.
@@ -14,17 +14,10 @@ export default [
      *
      * ESLint requires "ignores" key to be the only one in this object
      */
-    ignores: [
-      'dist/*',
-      'src-capacitor/*',
-      'src-cordova/*',
-      '.quasar/*',
-      'quasar.config.*.temporary.compiled*',
-      '/node_modules'
-    ]
+    // ignores: []
   },
 
-  ...pluginQuasar.configs.recommended(),
+  pluginQuasar.configs.recommended(),
   js.configs.recommended,
 
   /**
@@ -39,23 +32,19 @@ export default [
    * pluginVue.configs["flat/recommended"]
    *   -> Above, plus rules to enforce subjective community defaults to ensure consistency.
    */
-  ...pluginVue.configs[ 'flat/essential' ],
+  pluginVue.configs[ 'flat/essential' ],
 
+  {
+    files: ['**/*.ts', '**/*.vue'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports' }
+      ],
+    }
+  },
   // https://github.com/vuejs/eslint-config-typescript
-  ...vueTsEslintConfig({
-    // Optional: extend additional configurations from typescript-eslint'.
-    // Supports all the configurations in
-    // https://typescript-eslint.io/users/configs#recommended-configurations
-    extends: [
-      // By default, only the recommended rules are enabled.
-      'recommended'
-      // You can also manually enable the stylistic rules.
-      // "stylistic",
-
-      // Other utility configurations, such as 'eslintRecommended', (note that it's in camelCase)
-      // are also extendable here. But we don't recommend using them directly.
-    ]
-  }),
+  vueTsConfigs.recommendedTypeChecked,
 
   {
     languageOptions: {
@@ -99,4 +88,4 @@ export default [
       }
     }
   }
-]
+)
