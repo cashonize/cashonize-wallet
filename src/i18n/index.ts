d937163e853df444353ddacdf9fcbf01e9afc09a
Mathieu Geukens <mr-zwets@protonmail.com>
2026-02-17 08:33:49 +0100

    add german translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,11 +1,13 @@
 import en from './locales/en.json';
 import es from './locales/es.json';
 import fr from './locales/fr.json';
+import de from './locales/de.json';
 
 export const translations = {
   en,
   es,
-  fr
+  fr,
+  de
 };
 
 export const defaultLocale = 'en';
